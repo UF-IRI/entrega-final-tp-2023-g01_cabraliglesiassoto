d89d028fddcf13ceb8e10a90ec80fbc255983fd8
Lucasgiglesias <lucasg_iglesias@hotmail.com>
2023-12-06 17:44:45 -0300

    test 7

--- Proyecto/testing/test_Clases.cpp
+++ Proyecto/testing/test_Clases.cpp
@@ -1,2 +1,14 @@
+
 #include <Clases.h>
 #include <catch2/catch.hpp>
+TEST_CASE("Funciones Buscar Horario ","BH")
+{
+    sClases *clase = new sClases [5];
+    clase[0]={"1","Spinning",8.0,spinn,0};
+    clase[1]={"8","Yoga",11.0,yoga,0};
+    clase[2]={"10","Yoga",16.0,yoga,0};
+    clase[3]={"12","Pilates",8.0,pilates,0};
+    clase[4]={"15","Pilates",12.0,pilates,0};
+    REQUIRE (BuscarHorario("1",clase) == 8.0);
+    delete [] clase;
+}
--- Proyecto/testing/test_Cliente.cpp
+++ Proyecto/testing/test_Cliente.cpp
@@ -1,2 +1,25 @@
+
 #include "Cliente.h"
 #include <catch2/catch.hpp>
+
+TEST_CASE("Control Saldo","hola"){
+
+
+
+    Cliente *cliente = new Cliente;
+
+    // Asignar valores al cliente
+    cliente->id_cliente = "2";
+    cliente->Nombre = "Emilio";
+    cliente->Apellido = "Romero";
+    cliente->mail = "EmilioRomero@bolandia.iri";
+    cliente->telefono = "317-427-6829";
+    cliente->fechaNacimiento = "14-08-1998";
+    cliente->saldo = -3241;
+
+    REQUIRE( ControlEstado(cliente)==false );
+
+
+
+    delete cliente;
+}
